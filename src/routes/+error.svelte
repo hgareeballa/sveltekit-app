b94f1b5094f7a6cb7b4608365152895a65b70516
Elmuzamel <Hassan.Elmuzamel@pega.com>
2022-09-19 11:20:02 +0100

    update

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,5 +2,5 @@
   import { page } from "$app/stores";
 </script>
 
-<h1>Page not found</h1>
+<h1>Error Page</h1>
 <h1>{$page.status}: {$page.error.message}</h1>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,34 @@
 
 <div class="container">
   <HeaderPage {current} />
-  {#key $page}
-    <div align="center" in:fly={{ y: 200, duration: 2000 }}>
-      <slot />
+  <div id="page-container">
+    <div id="content-wrap">
+      {#key $page}
+        <div align="center" in:fly={{ y: 200, duration: 2000 }}>
+          <slot />
+        </div>
+      {/key}
     </div>
-  {/key}
-
-  <FooterPage />
+    <footer id="footer">
+      <FooterPage />
+    </footer>
+  </div>
 </div>
+
+<style>
+  #page-container {
+    position: relative;
+    min-height: 75vh;
+  }
+
+  #content-wrap {
+    padding-bottom: 2.5rem; /* Footer height */
+  }
+
+  #footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 2.5rem; /* Footer height */
+  }
+</style>
